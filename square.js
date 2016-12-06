61adf6f32ad2540f2ae450782aae19aca1b1a88b
SMTSOFTWARE\sebastian.glowacki <sebastian.glowacki@smtsoftware.com>
2016-12-06 18:19:35 +0100

    highlighting squares

--- square.js
+++ square.js
@@ -1,3 +1,46 @@
 /**
  * Created by sebastian.glowacki on 06.12.2016.
  */
+function getRandomNumbers(n) {
+
+    var randomNumbers = [];
+    for(var i = 0; i < n; i++)
+    {
+        var randomNumber = Math.ceil(Math.random() * 4);
+         randomNumbers.push(randomNumber)
+    }
+
+    return randomNumbers;
+}
+
+function hilightSquares(arrayNumbers, i) {
+     if(i < arrayNumbers.length) {
+         hilightSingleSquare(arrayNumbers[i]);
+         setTimeout(function() {
+             hideHilight(arrayNumbers[i]);
+             i++;
+             hilightSquares(arrayNumbers, i);
+         }, 1000);
+     }
+ }
+
+ function hilightSingleSquare(n) {
+     $('.container').find("[data-number=" + n + "]").css({
+         'background-color': "yellow"
+     });
+ }
+
+ function hideHilight(n) {
+     $('.container').find("[data-number=" + n + "]").css({
+         'background': "none"
+     });
+ }
+
+ var randomNumbers = getRandomNumbers(5);
+hilightSquares(randomNumbers, 0);
+
+console.log(randomNumbers);
+
+$('.square').click(function(e){
+    console.log(e);
+});
